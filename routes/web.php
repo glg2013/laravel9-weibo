b2ba6c84e74583429a2f30c9d69acf22db230f6d
glg2013 <57412960@qq.com>
2023-04-24 09:10:24 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
-Route::get('/', [StaticPagesController::class, 'home']);
-Route::get('/help', [StaticPagesController::class, 'help']);
-Route::get('/about', [StaticPagesController::class, 'about']);
+Route::get('/', [StaticPagesController::class, 'home'])->name('home');
+Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
+Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
