3e685741cacd6346040370a95cb648978ab0e445
glg2013 <57412960@qq.com>
2023-04-28 08:33:28 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,7 +68,10 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderByDesc('created_at');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        //return $this->statuses()->orderByDesc('created_at');
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderByDesc('created_at');
     }
 
     /**
